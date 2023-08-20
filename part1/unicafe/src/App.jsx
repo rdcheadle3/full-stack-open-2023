315a4fe59f782506443dee95a1ac7cdc292cc4e0
Robert <109326004+rdcheadle3@users.noreply.github.com>
2023-08-19 22:10:36 -0400

    feat: Add feedback tracking and display functionality
    - Refactored the App component to use a single 'feedback' state object
      for better organization and management of user feedback counts.
    
    - Created Header, Button, and Display components to enhance code
      modularity and readability.
    
    - Added event handlers to track clicks on the feedback buttons and
      update the 'feedback' state accordingly.
    
    - Added console logs for debugging purposes to track button clicks
      and updated state.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,43 @@
 import { useState } from "react";
 
-const App = () => {
-	// save clicks of each button to its own state
-	const [good, setGood] = useState(0);
-	const [neutral, setNeutral] = useState(0);
-	const [bad, setBad] = useState(0);
+const Header = ({ text }) => <h1>{text}</h1>;
 
-	return <div>code here</div>;
+const Button = ({ handleClick, text }) => (
+	<button onClick={handleClick}>{text}</button>
+);
+
+const Display = ({ value }) => <div>{value}</div>;
+
+const App = () => {
+	const [feedback, setFeedback] = useState({
+		good: 0,
+		neutral: 0,
+		bad: 0,
+	});
+
+	const handleButtonClicked = (type) => {
+		console.log(`${type} clicked`);
+
+		setFeedback({ ...feedback, [type]: feedback[type] + 1 });
+
+		console.log(feedback);
+	};
+
+	return (
+		<div>
+			<Header text="give feedback" />
+			<Button handleClick={() => handleButtonClicked("good")} text="good" />
+			<Button
+				handleClick={() => handleButtonClicked("neutral")}
+				text="neutral"
+			/>
+			<Button handleClick={() => handleButtonClicked("bad")} text="bad" />
+			<Header text="statistics" />
+			<Display value={feedback.good} />
+			<Display value={feedback.neutral} />
+			<Display value={feedback.bad} />
+		</div>
+	);
 };
 
 export default App;
